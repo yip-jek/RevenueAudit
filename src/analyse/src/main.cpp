37b4d29f953a18378194cbfe31499c69499a9f93
ej <yiyijie@foxmail.com>
2016-04-07 00:01:44 +0800

    modified:  src/analyse/src/main.cpp

--- src/analyse/src/main.cpp
+++ src/analyse/src/main.cpp
@@ -1,20 +1,8 @@
 #include <iostream>
-#include "test.h"
-#include "base.h"
+#include "analyse.h"
 
 int main(int argc, char* argv[])
 {
-	std::cout << "[main] " << argv[0] << " ... OK!" << std::endl;
-
-	Test tt;
-	std::cout << "TEST> before [INIT] value: " << tt.GetVal() << std::endl;
-	tt.Init();
-	std::cout << "TEST> after [INIT] value: " << tt.GetVal() << std::endl;
-
-	std::cout << "base static val: " << Base::S_GetVal(1) << std::endl;
-
-	Base b;
-	std::cout << "base val: " << b.GetInterVal() << std::endl;
 	return 0;
 }
 
